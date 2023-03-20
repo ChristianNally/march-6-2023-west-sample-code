99ed08a0aec577fad4fa4897d6c06367a77dd748
Christian Nally <sticksallison@gmail.com>
2023-03-20 09:41:06 -0700

    promises in-class edits

--- w02d4/solution/promises/intro.js
+++ w02d4/solution/promises/intro.js
@@ -5,9 +5,9 @@ const promise = returnPromise('first promise', 4444);
 console.log('promise:',promise);
 
 console.log('first');
-console.log('second');
+// console.log('second');
 
-// Not the best way to do this
+// // Not the best way to do this
 // setTimeout(()=>{
 //   console.log('promise 6 seconds later:',promise);
 // },6000);
@@ -19,10 +19,9 @@ console.log('second');
 promise
   .then((data) => {
     console.log('third');
-    // const newData = 'another thing';
-    // console.log("newData:",newData);
     console.log('data:', data);
-  }) // <--- that is just a function call. no magic there!
-  .then(() => {
-    console.log('it is alive!');
+    return returnPromise('second promise', 2000);
+  })
+  .then((data) => {
+    console.log('data (the second version)', data);
   });
